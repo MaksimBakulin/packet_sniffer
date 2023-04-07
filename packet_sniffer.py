265cf434d46f67d9b1c9f9de3b7a9a7a13f462c4
root <mbakulin103@gmail.com>
2023-04-07 12:51:28 -0400

    commit_2

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+from scapy.layers import http
 
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=procces_sniffed_packet)
 
 def procces_sniffed_packet(packet):
-    print(packet)
+    if packet.haslayer(http.HTTPRequest):
+        if packet.haslayer(scapy.Raw):
+            print(packet[scapy.Raw].load)
 
 sniff('eth0')
